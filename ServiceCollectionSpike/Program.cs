608763e7fae5994f59eca5e22f9a691924a5c63f
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-07-26 19:35:14 +0900

    Add project files.

---
+++ ServiceCollectionSpike/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServiceCollectionSpike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
